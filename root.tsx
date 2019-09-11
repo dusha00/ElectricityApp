9b0cffd6fe8645a53c051e4a9e541fd3e6ddbe4d
Bob Du <DUXBX@legal.regn.net>
2019-09-11 10:20:13 +0800

    integrated codepush

--- root.tsx
+++ root.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import { Provider } from "mobx-react";
 import AppContainer from "./navigation";
 import service from "./src/service/Service";
+import codePush from "react-native-code-push"
 
 export default function setup() {
   class Root extends React.Component {
+    componentDidMount(){
+      codePush.sync({
+        updateDialog: {
+          appendReleaseDescription: true,
+          descriptionPrefix: '\n\n更新内容：\n',
+          title: '更新',
+          mandatoryUpdateMessage: '',
+          mandatoryContinueButtonLabel: '更新',
+        },
+        mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+        deploymentKey: ''
+      }); 
+    }
     render() {
       return (
         <Provider {...service}>
