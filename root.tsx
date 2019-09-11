72709fa1aa77e648faf16415ee01b193788f7bf2
Bob Du <DUXBX@legal.regn.net>
2019-09-11 13:34:46 +0800

    update

--- root.tsx
+++ root.tsx
@@ -6,19 +6,19 @@ import codePush from "react-native-code-push"
 
 export default function setup() {
   class Root extends React.Component {
-    componentDidMount(){
-      codePush.sync({
-        updateDialog: {
-          appendReleaseDescription: true,
-          descriptionPrefix: '\n\n更新内容：\n',
-          title: '更新',
-          mandatoryUpdateMessage: '',
-          mandatoryContinueButtonLabel: '更新',
-        },
-        mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
-        deploymentKey: ''
-      }); 
-    }
+    // componentDidMount(){
+    //   codePush.sync({
+    //     updateDialog: {
+    //       appendReleaseDescription: true,
+    //       descriptionPrefix: '\n\n更新内容：\n',
+    //       title: '更新',
+    //       mandatoryUpdateMessage: '',
+    //       mandatoryContinueButtonLabel: '更新',
+    //     },
+    //     mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+    //     deploymentKey: 'ZBKfbjFkmQDnFPMutRVNhXk_IpOs7c43d71b-ded3-42cf-9912-e95f6bdc5864'
+    //   }); 
+    // }
     render() {
       return (
         <Provider {...service}>
